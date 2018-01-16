aac3a3bcbf7b7998a6dc2ff963ec9bcfb0ade62d
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-01-16 16:36:12 +0000

    Add web_helper file

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -9,10 +9,7 @@ end
 describe "submitting player names", :type => :feature do
 
    it "should store player names" do
-   visit "/"
-   fill_in "player_1", :with => "Giada"
-   fill_in "player_2", :with => "Tom"
-   click_button "Submit"
+    sign_in_and_play
    expect(page).to have_content("Player 1: Giada Player 2: Tom")
   end
 end
--- spec/features/hp_spec.rb
+++ spec/features/hp_spec.rb
@@ -1,7 +1,7 @@
 describe "checking player hp", :type => :feature do
 
    it "show us play 2's health" do
-   visit "/play"
+   sign_in_and_play
    click_button "Player_2_health"
    expect(page).to have_content("Player 2's health = 80")
   end
