ecad67f2f47239cb4d365f46ebc1065783386472
Tom Grand <thomas.a.grand@gmail.com>
2018-01-16 14:44:17 +0000

    add player names to battle

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -4,3 +4,15 @@ describe "web page content", :type => :feature do
     expect(page).to have_content("Testing infrastructure working!")
   end
 end
+
+
+describe "submitting player names", :type => :feature do
+   
+   it "should store player names" do
+   visit "/" 
+   fill_in "player_1", :with => "Giada"  
+   fill_in "player_2", :with => "Tom"
+   click_button "Submit"
+   expect(page).to have_content("Player 1: Giada Player 2: Tom")
+  end
+end
